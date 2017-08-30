d8019760ec84f769e7c1210cb2300edccbe3ddd6
Gregory Nowakowski <gregnb@gmail.com>
2017-08-30 10:57:50 -0400

    updates to dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,19 @@
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
   plugins: [
     babel({
       "presets": ["es2015-rollup"]
+    }),
+    nodeResolve({
+      jsnext: true
     })
   ],
   output: {
     file: 'lib/index.js',
     format: 'cjs'
-  }
+  },
+  sourceMap: true
 };
