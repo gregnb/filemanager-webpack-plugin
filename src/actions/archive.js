5b94db977753fe8af696dae68850f1b09872a611
Patrick Nobbe <patrick.nobbe@spatial-eye.com>
2018-08-29 15:29:14 +0200

    Fix archive including destination when destination is child of archive source

--- src/actions/archive.js
+++ src/actions/archive.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const archiver = require('archiver');
 
@@ -25,7 +25,7 @@ function archiveAction(command, options) {
       const fileRegex = /(\*|\{+|\}+)/g;
       const matches = fileRegex.exec(command.source);
 
-      const isGlob = matches !== null ? true : false;
+      const isGlob = matches !== null;
 
       fs.lstat(command.source, (sErr, sStats) => {
         const output = fs.createWriteStream(command.destination);
@@ -34,10 +34,14 @@ function archiveAction(command, options) {
         archive.on('error', err => reject(err));
         archive.pipe(output);
 
-        if (isGlob) archive.glob(command.source, command.options.globOptions || {});
-        else if (sStats.isFile()) archive.file(command.source, { name: path.basename(command.source) });
-        else if (sStats.isDirectory()) archive.directory(command.source, false);
+        // Exclude destination file from archive
+        const destFile = path.basename(command.destination);
+        const globOptions = Object.assign({ ignore: destFile }, (command.options.globOptions || {}) );
 
+        if (isGlob) archive.glob(command.source, globOptions);
+		else if (sStats.isFile()) archive.file(command.source, { name: path.basename(command.source) });
+        else if (sStats.isDirectory()) archive.glob(`${ command.source }/*/**`, globOptions);
+        
         archive.finalize().then(() => resolve());
       });
     });
