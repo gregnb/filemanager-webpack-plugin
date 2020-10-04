fab01dcd778b7540987ba6e9527dc6c060fafbf9
sibiraj-s <dev.sibiraj@outlook.com>
2020-10-04 17:01:32 +0530

    Add tests for globs in delete actions

--- tests/delete.test.js
+++ tests/delete.test.js
@@ -34,6 +34,26 @@ test.serial('should delete file when array of strings provided in delete functio
   t.pass();
 });
 
+test.serial('should support glob', async (t) => {
+  await writeFile('./testing/deletable-file.js', '');
+  await writeFile('./testing/deletable-file2.js', '');
+  await writeFile('./testing/deletable-file3.js', '');
+
+  const config = {
+    onStart: {
+      delete: ['./testing/*'],
+    },
+  };
+
+  new FileManagerPlugin(config).apply(compiler);
+  await compile(compiler);
+
+  t.false(existsSync('./testing/deletable-file.js'));
+  t.false(existsSync('./testing/deletable-file2.js'));
+  t.false(existsSync('./testing/deletable-file3.js'));
+  t.pass();
+});
+
 test.serial(
   'should fail webpack build when delete function receives anything other than an array of strings',
   async (t) => {
