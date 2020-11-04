d917f89f2827b0b705f7d251613ae5f01cebcc96
sibiraj-s <dev.sibiraj@outlook.com>
2020-11-04 19:36:28 +0530

    Fix tests

--- tests/multi-actions.test.js
+++ tests/multi-actions.test.js
@@ -27,8 +27,8 @@ test('should execute given actions in an event', async (t) => {
   const config = {
     events: {
       onEnd: {
+        mkdir: ['testing-multiaction-dir'],
         copy: [{ source: 'dist/index.html', destination: 'testing-multiple-actions/index.copied.html' }],
-        move: [{ source: 'testing-multiple-actions/', destination: 'testing-multi-action-dir1/' }],
       },
     },
   };
@@ -37,7 +37,7 @@ test('should execute given actions in an event', async (t) => {
   new FileManagerPlugin(config).apply(compiler);
   await compile(compiler);
 
+  t.true(existsSync('./testing-multiaction-dir'));
   t.true(existsSync('./testing-multiple-actions/index.copied.html'));
-  t.true(existsSync('./testing-multi-action-dir1/index.html'));
   t.pass();
 });
