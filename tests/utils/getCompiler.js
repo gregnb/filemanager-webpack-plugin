4354e866b94b9be0d66262c2754f7b2c4c65beb8
sibiraj-s <dev.sibiraj@outlook.com>
2021-03-07 13:25:35 +0530

    Drop webpack 4

--- tests/utils/getCompiler.js
+++ tests/utils/getCompiler.js
@@ -6,12 +6,6 @@ import getFixtruesDir from './getFixturesDir';
 
 const fixturesDir = getFixtruesDir();
 
-const webpack5Options = {
-  clean: true,
-};
-
-const isWebpack5 = typeof webpack.version === 'string' ? parseInt(webpack.version) === 5 : false;
-
 const getCompiler = () => {
   const compiler = webpack({
     context: fixturesDir,
@@ -20,7 +14,7 @@ const getCompiler = () => {
     output: {
       path: path.resolve(fixturesDir, 'dist'),
       filename: 'js/bunlde-[contenthash].js',
-      ...(isWebpack5 ? webpack5Options : {}),
+      clean: true,
     },
     plugins: [new HTMLPlugin()],
   });
