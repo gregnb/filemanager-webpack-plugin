051168b36c50ec20259f4f01b17bbf9894df6c00
sibiraj-s <dev.sibiraj@outlook.com>
2021-03-07 13:12:07 +0530

    Fix tests

--- tests/utils/getCompiler.js
+++ tests/utils/getCompiler.js
@@ -6,6 +6,12 @@ import getFixtruesDir from './getFixturesDir';
 
 const fixturesDir = getFixtruesDir();
 
+const webpack5Options = {
+  clean: true,
+};
+
+const isWebpack5 = typeof webpack.version === 'string' ? parseInt(webpack.version) === 5 : false;
+
 const getCompiler = () => {
   const compiler = webpack({
     context: fixturesDir,
@@ -14,7 +20,7 @@ const getCompiler = () => {
     output: {
       path: path.resolve(fixturesDir, 'dist'),
       filename: 'js/bunlde-[contenthash].js',
-      clean: true,
+      ...(isWebpack5 ? webpack5Options : {}),
     },
     plugins: [new HTMLPlugin()],
   });
