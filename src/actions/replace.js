43b89a4c048daf9a30418ba54271733a94c01c41
Anderson Aguiar <anderson.aguiar@mcx.com.br>
2022-12-23 09:37:42 -0300

    feat: check if file exist before replace

--- src/actions/replace.js
+++ src/actions/replace.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs, { existsSync } from 'fs';
 import pExec from '../utils/p-exec.js';
 
 const replaceAction = async (tasks, options) => {
@@ -7,27 +7,33 @@ const replaceAction = async (tasks, options) => {
   logger.debug(`processing replace tasks. tasks: ${tasks}`);
 
   await pExec(runTasksInSeries, tasks, async (task) => {
-    fs.readFile(task.source, 'utf8', function (err, data) {
-      if (err) {
-        logger.error(`unable to replace ${task.source}, ${err}`);
-      }
+    const file = task.source;
 
-      task.mutations.forEach((mutation) => {
-        let iterations = mutation.iterations ? mutation.iterations : 1;
-        for (let index = 0; index < iterations; index++) {
-          data = data.replace(mutation.pattern, mutation.replacement);
-        }
-      });
-
-      fs.writeFile(task.source, data, 'utf8', function (err) {
+    if (existsSync(file)) {
+      fs.readFile(file, 'utf8', function (err, data) {
         if (err) {
-          logger.error(`unable to write ${task.source}, ${err}`);
+          logger.error(`unable to replace ${file}, ${err}`);
         }
+
+        task.mutations.forEach((mutation) => {
+          let iterations = mutation.iterations ? mutation.iterations : 1;
+          for (let index = 0; index < iterations; index++) {
+            data = data.replace(mutation.pattern, mutation.replacement);
+          }
+        });
+
+        fs.writeFile(file, data, 'utf8', function (err) {
+          if (err) {
+            logger.error(`unable to write ${file}, ${err}`);
+          }
+        });
       });
-    });
+    } else {
+      logger.error(`unable to find ${file}`);
+    }
   });
 
-  logger.debug(`replace tasks complete. tasks: ${tasks}`);
+  logger.debug(`move tasks complete. tasks: ${tasks}`);
 };
 
 export default replaceAction;
